3a56b191e7fe8eaf882fb6bcadc0cb9d71ad1f79
MadRobin13 <madrobin1340@gmail.com>
2024-06-02 23:55:31 -0400

    create, read and doomsday

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const {MongoClient} = require('mongodb');
 const uri = "mongodb+srv://mongo:mongo@java-coffee.06pqnyw.mongodb.net/?retryWrites=true&w=majority&appName=Java-Coffee";
-
+const client = new MongoClient(uri);
 
 async function main() {
     try {
-        const client = new MongoClient(uri);
         await client.connect();
 
-        await client.db("DishDiscover").collection("Recipes").insertOne({name: "Burger", recipe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales enim ac condimentum blandit. Curabitur iaculis nibh molestie eros dapibus, sit amet pharetra massa imperdiet.", ingredients: ["bun", "patty", "lettuce", "tomato", "onion"]});
+        await addRecipe('John', 'Pasta', 'Boil water, Add pasta, Cook for 10 minutes', 'Pasta, Water');
+
+        await getRecipe('Pasta', 'John');
 
     } catch(e) {
         console.error(e);
@@ -17,3 +18,43 @@ async function main() {
 }
 
 main().catch(console.error);
+
+async function addRecipe(creator, food, steps, ings) {
+    let ingredients = [];
+    let recipe = [];
+    step = ''
+    let ing = '';
+
+    for (let i = 0; i < ings.length; i++) {
+        ing += ings[i];
+        if (ings[i] == ',' || i == ings.length - 1) {
+            if (i != ings.length - 1) {
+                ing = ing.slice(0, ing.length - 1);
+            }
+            ingredients.push(ing);
+            ing = '';
+        }
+    }
+
+    for (let j = 0; j < steps.length; j++) {
+        step += steps[j];
+        if (steps[j] == ',' || j == steps.length - 1) {
+            if (j != steps.length - 1) {
+                step = step.slice(0, step.length - 1);
+            }
+            recipe.push(step);
+            step = '';
+        }
+    }
+
+    await client.db("DishDiscover").collection("Recipes").insertOne({creator: creator, name: food, recipe: recipe, ingredients: ingredients});
+} 
+
+async function getRecipe(food, creator) {
+    const recipe = await client.db("DishDiscover").collection("Recipes").findOne({name: food, creator: creator});
+    console.log(recipe);
+}
+
+async function doomsday() {
+    await client.db("DishDiscover").collection("Recipes").deleteMany({});
+}
